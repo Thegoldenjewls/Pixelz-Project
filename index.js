5dd51fd0bc2c216d17b61d6d4631c4c1a70e103e
Thegoldenjewls <Julianlucoronado@gmail.com>
2022-12-06 13:44:23 -0800

    12/6/22 13:44

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
-const express = require('express')
-const app = express ()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/',(req, res) => {
-    res.send('Hello bumboclot')
+app.set('view engine', 'ejs')
+
+
+app.get('/', (req, res) => {
+    res.render('index', {firstName: 'Julian'})
 })
-app.set ('view engine,ejs')
-    app.listen(port, () => {
-        console.log(`Server started at ${port}`)
-    })
+
+app.get('/apple', (req, res) => {
+    res.send('This is apple')
+})
+
+let users = [
+    {
+        id: 1,
+        username: 'Julian',
+        age: 22
+    },
+    {
+        id: 2,
+        username: 'ta7',
+        age: 33
+    },
+    {
+        id: 3,
+        username: 'jumpman23',
+        age: 55
+    }
+]
+
+app.get('/users', (req, res) => {
+    res.render('users', { users })
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+    for (u of users){
+        if (u.id == id){
+            res.render('user', {user: u})
+        }
+    }
+    res.send({error: `User with id ${id} does not exist`})
+})
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`)
+})
